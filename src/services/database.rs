6ebc9da3dfac65effcc1f2ff8cc968e978440ae9
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-12-02 18:17:24 +0000

    knowledge score service implemented

---
+++ src/services/database.rs
@@ -0,0 +1,10 @@
+use std::{env, error::Error};
+
+
+pub async fn get_connection_string() -> Result<String, Box<dyn Error>> {
+    let user = env::var("POSTGRES_USER")?;
+    let db = env::var("POSTGRES_DB")?;
+    let pass = env::var("POSTGRES_PASSWORD")?;
+    let ip = env::var("POSTGRES_PASSWORD")?;
+    Ok(format!("host={} user={} db={} password={}",ip,user,db,pass).to_string())
+}
--- src/services/knowledge_service.rs
+++ src/services/knowledge_service.rs
@@ -1,4 +1,5 @@
-use crate::structs::{knowledge_score_request::KnowledgeScoreRequest, knowledge_score_update::KnowledgeScoreUpdate};
+use postgres::{Client,NoTls};
+use crate::{services::database::get_connection_string, structs::{knowledge_score_request::KnowledgeScoreRequest, knowledge_score_update::KnowledgeScoreUpdate}};
 
 #[derive(thiserror::Error, Debug)]
 pub enum KnowledgeError {
@@ -13,7 +14,22 @@ pub enum KnowledgeError {
 }
 
 pub async fn get_knowledge_score(skill_request: KnowledgeScoreRequest) -> Result<f64, KnowledgeError> {
-    Ok(0.1)
+    let connection_string = match get_connection_string().await {
+        Ok(connection) => connection,
+        Err(e) => return Err(KnowledgeError::Database(format!("Failed to get database environment variables: {e}")))
+    };
+
+    let mut client = match Client::connect(&connection_string, NoTls) {
+        Ok(client ) => client,
+        Err(e) => return Err(KnowledgeError::Database(format!("Failed to create client: {}", e)))
+    };
+
+    let knowledge_score: f64 =  match client.query_one("SELECT progression FROM progression WHERE user_id=$1 AND skill_id=$2", &[&skill_request.student_id, &skill_request.skill_id]){
+        Ok(row) => row.get(0),
+        Err(e) => return Err(KnowledgeError::Database(format!("Failed to get knowledge score: {}", e))) 
+    };
+
+    Ok(knowledge_score)
 }
 
 pub async fn update_knowledge_score(knowledge_update: KnowledgeScoreUpdate) -> Result<(), KnowledgeError> {
