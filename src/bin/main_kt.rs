c478b1bcfd3968cd5280cef29c2b791e1a6d239e
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-12-02 11:15:08 +0000

    added auto docs

--- src/bin/main_kt.rs
+++ src/bin/main_kt.rs
@@ -2,13 +2,31 @@ use axum::{
     routing::get,
     Router,
 };
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World! (Knowledge Tracing)" }));
+    #[derive(OpenApi)]
+    #[openapi(paths(hello_world), components(schemas()), tags())]
+    struct ApiDoc;
 
-    // run our app with hyper, listening globally on port 3000
+    let app = Router::new()
+        .merge(SwaggerUi::new("/docs").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .route("/", get(hello_world));
+
+    // run on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[utoipa::path(
+    get,
+    path = "/",
+    responses(
+        (status = 200, description = "Hello world response")
+    )
+)]
+async fn hello_world() -> &'static str {
+    "Hello, World! (Knowledge Tracing)"
 }
