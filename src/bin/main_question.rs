dad0e470da94133f9a531cadbd7b022f278425b9
William Shepherdson <48570554+WilliamShepherdson@users.noreply.github.com>
2025-12-02 12:33:28 +0000

    updated hello world to life check

--- src/bin/main_question.rs
+++ src/bin/main_question.rs
@@ -11,12 +11,12 @@ use utoipa_swagger_ui::SwaggerUi;
 async fn main() {
 
     #[derive(OpenApi)]
-    #[openapi(paths(hello_world), components(schemas()), tags())]
+    #[openapi(paths(pong), components(schemas()), tags())]
     struct ApiDoc;
 
     let app = Router::new()
         .merge(SwaggerUi::new("/docs").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .route("/", get(hello_world));
+        .route("/", get(pong));
 
     // run on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -25,11 +25,11 @@ async fn main() {
 
 #[utoipa::path(
     get,
-    path = "/",
+    path = "/ping",
     responses(
-        (status = 200, description = "Hello world response")
+        (status = 200, description = "Life check")
     )
 )]
-async fn hello_world() -> &'static str {
-    "Hello, World! (Question Generation)"
+async fn pong() -> &'static str {
+    "pong"
 }
