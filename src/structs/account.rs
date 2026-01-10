0fbbd0abc3cc5af7511d2c2b854b1b0133c58c77
williamshepherdson <w.shepo@gmail.com>
2026-01-10 13:39:33 +0000

    added methods for account creation and passwords

---
+++ src/structs/account.rs
@@ -0,0 +1,6 @@
+pub struct Account {
+    pub first_name: String,
+    pub last_name: String,
+    pub username: String,
+    pub password: String
+}
---
+++ src/structs/sign_in.rs
@@ -0,0 +1,4 @@
+pub struct SignIn {
+    pub username: String,
+    pub password: String
+}
